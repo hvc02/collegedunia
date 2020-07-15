e3315b99f5e1518f56555292aa78b4cfe5330a96
Harivansh <harichanda02@gmail.com>
2020-07-15 21:50:38 +0530

    Updates

---
+++ precache-manifest.a781831b8fdb2f41fd16b31185c568a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af4541a57daeecd9abcb7bce9d04662",
+    "url": "/collegedunia/index.html"
+  },
+  {
+    "revision": "12e503e6da38a3c9ab88",
+    "url": "/collegedunia/static/css/main.5ef10e2f.chunk.css"
+  },
+  {
+    "revision": "2b7a2801a12f4c5557c6",
+    "url": "/collegedunia/static/js/2.d7a0e1c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collegedunia/static/js/2.d7a0e1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e503e6da38a3c9ab88",
+    "url": "/collegedunia/static/js/main.7ce8b5d4.chunk.js"
+  },
+  {
+    "revision": "e6ac393a53549779ac85",
+    "url": "/collegedunia/static/js/runtime-main.311d44eb.js"
+  },
+  {
+    "revision": "0888425abd5aaed8ae7401daa0395566",
+    "url": "/collegedunia/static/media/DINPro-Medium tr.0888425a.ttf"
+  },
+  {
+    "revision": "d75d7f70aa160104a45a743eae7a8a7e",
+    "url": "/collegedunia/static/media/MYRIADPRO-REGULAR.d75d7f70.OTF"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/collegedunia/static/media/college_02.619ee97e.jpg"
+  }
+]);
